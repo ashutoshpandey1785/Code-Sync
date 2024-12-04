d6db559af82ebf7ca57613855589d852af1b62b8
jarvisonline <98217852+jarvisonline@users.noreply.github.com>
2024-12-04 18:44:39 +0530

    socket connection fixed 💣

--- src/socket.js
+++ src/socket.js
@@ -1,12 +1,15 @@
 import { io } from "socket.io-client";
 
 export const initSocket = async () => {
-    const options = {
-        'force new connection': true,
-        reconnectionAttempt: 'Infinity',
-        timeout: 10000,
-        transports: ['websocket'],
-    };
+  const options = {
+    "force new connection": true, // Always create a new connection
+    reconnectionAttempts: "Infinity", // Retry indefinitely
+    timeout: 10000, // Connection timeout (10 seconds)
+    transports: ["websocket"], // Use WebSocket as the transport
+  };
 
-    return io(process.env.REACT_APP_BACKEND_URL, options);
+  return io(
+    process.env.REACT_APP_BACKEND_URL || "http://localhost:5000",
+    options
+  );
 };
