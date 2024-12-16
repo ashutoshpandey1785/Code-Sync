ec93c16a1b9abb8f68dd0f9637638f85fb1aa0c4
ashutoshpandey1785 <ashutoshpandey1785@gmail.com>
2024-12-16 15:38:10 +0530

    build Boom 👊👊🔥

--- server.js
+++ server.js
@@ -8,10 +8,12 @@ const ACTIONS = require('./src/Actions');
 const server = http.createServer(app);
 const io = new Server(server);
 
-// app.use(express.static('build'));
-// app.use((req, res, next) => {
-//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// })
+// build
+app.use(express.static('build'));
+app.use((req, res, next) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+})
+// build end
 
 const userSocketMap = {};
 
