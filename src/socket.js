bb475847bea92cb1fb34e33abf8758f7bd6262a1
ashutoshpandey1785 <ashutoshpandey1785@gmail.com>
2024-12-16 16:05:57 +0530

    boom

--- src/socket.js
+++ src/socket.js
@@ -9,7 +9,7 @@ export const initSocket = async () => {
   };
 
   return io(
-    process.env.REACT_APP_BACKEND_URL || "http://localhost:5000",
+    process.env.REACT_APP_BACKEND_URL || "http://localhost:5000" || "https://code-sync-f4ev.onrender.com",
     options
   );
 };
